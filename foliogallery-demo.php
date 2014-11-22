b8eaf75a7fbf95eef5ecbed9455dcb09b3ed48f2
integrii <integrii@gmail.com>
2014-11-22 02:12:21 -0800

    1.3 rewrite merge

--- foliogallery-demo.php
+++ foliogallery-demo.php
@@ -1,7 +1,5 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Image Gallery By FolioPages.com</title>
 <style type="text/css">
 body {
@@ -15,20 +13,28 @@ color:#222;
 <link type="text/css" rel="stylesheet" href="foliogallery/foliogallery.css" />
 <link type="text/css" rel="stylesheet" href="colorbox/colorbox.css" />
 <script type="text/javascript" src="foliogallery/jquery.1.11.js"></script>
+<script type="text/javascript" src="foliogallery/foliogallery.js"></script>
 <script type="text/javascript" src="colorbox/jquery.colorbox-min.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-    // initiate colorbox
-	$(".albumpix").colorbox({rel:'albumpix', maxWidth:'98%', maxHeight:'98%', slideshow:true, slideshowSpeed:3500, slideshowAuto:false});
-});	
-</script>
 </head>
 <body>
 
 <br />
 <br />
 
-<?php $_REQUEST['fullalbum']=1; include 'foliogallery.php'; ?>
+<!-- display images located in "Lake Tahoe" subfolder and paginate in 3 item blocks -->
+<div id="folioGallery2" class="folioGallery" title="Lake Tahoe"><div class="numPerPage" title="3"></div></div>
+
+<br />
+<br /> 
+
+<!-- display images located in "Las Vegas Strip" subfolder and paginate in 3 item blocks -->
+<div id="folioGallery3" class="folioGallery" title="Las Vegas Strip"><div class="numPerPage" title="3"></div></div>
+
+<br />
+<br />
+
+<!-- display list of folders in "albums" folder and paginate in 6 item blocks -->
+<div id="folioGallery" class="folioGallery"><div class="numPerPage" title="3"></div></div>
 
 <br />
 <br />
